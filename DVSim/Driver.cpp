3e1785eb53646d3ed78038e9c3981d0a4b8b09ac
Atomsphere <m.w.maupin@gmail.com>
2017-11-08 03:52:59 -0600

    Most functionality setup. Needs a solution for the boolean return on receive DVPacket to track what round a node was last changed in.

--- DVSim/Driver.cpp
+++ DVSim/Driver.cpp
@@ -12,9 +12,49 @@ int main(int argc, char *argv[]) {
 
 	int rounds = atoi(argv[1]);
 	ifstream input(argv[0]);
+	vector<int> tempNodes;
+	Node * network;
+	int * changeTracker;
+	int node1, node2, distance, index, netSize = 0;
+	vector<Packet> dvPacket;
+	vector<int> neighbors;
 
+	//preprocess file for number of nodes
+	while (input >> node1) {
+		input >> node2 >> distance;
+		tempNodes.push_back(node1);
+		tempNodes.push_back(node2);
+	}
+	for (int i = 0; i < tempNodes.size(); ++i) {
+		if (netSize < tempNodes[i])
+			netSize = tempNodes[i];
+	}
+	network = new Node [netSize + 1];
+	changeTracker = new int[netSize + 1];
+	input.clear();
+	input.seekg(0, input.beg);
+	//end preprocess
+
+	//initialize network
+	while (input >> node1) {
+		input >> node2 >> distance;
+		network[node1].addNeighbor(node2, distance);
+		network[node2].addNeighbor(node1, distance);
+	}
+
+	//primary loop
 	for (int i = 0; i < rounds; ++i) {
-
+		for (int j = 0; j <= netSize; ++j) {
+			network[j].createDV();
+		}
+		for (int j = 0; j <= netSize; ++j) {
+			dvPacket = network[j].sendDV();
+			neighbors = network[j].getNeighbors();
+			for (int k = 0; k < neighbors.size(); ++k) {
+				network[neighbors[k]].receiveDV(dvPacket, j);
+			}
+		}
 	}
 
 }
+
--- DVSim/Node.cpp
+++ DVSim/Node.cpp
@@ -1,6 +1,11 @@
 #include <vector>
 using namespace std;
 
+struct Packet {
+	int cost;
+	int destination;
+};
+
 class Node {
 private:
 	struct Route {
@@ -8,22 +13,18 @@ private:
 		int destination;
 		int nextHop;
 	};
-	struct Packet {
-		int cost;
-		int destination;
-	};
-
-	int id;
-	vector<Route> neighbors;
+	vector<int> neighbors;
 	vector<Packet> dvPacket;
 	vector<Route> routingTable;
-	vector<int> convergence;
+	int convergence;
 
 public:
 	bool receiveDV(vector<Packet> dv, int id);
+	vector<Packet> sendDV() { return dvPacket; }
 	void createDV();
-	Node(int identity) { id = identity; }
-
+	Node() { }
+	void addNeighbor(int, int);
+	vector<int> getNeighbors() { return neighbors; }
 };
 
 void Node::createDV() {
@@ -37,5 +38,50 @@ void Node::createDV() {
 }
 
 bool Node::receiveDV(vector<Packet> dv, int id) {
+	int cost;
+	bool found, changed;
+	Route route;
 
+	changed = false;
+	for (int i = 0; i < routingTable.size(); ++i) {
+		if (routingTable[i].destination == id) {
+			cost = routingTable[i].cost;
+		}
+	}
+
+	for (int i = 0; i < dv.size(); ++i) {
+		found = false;
+		for (int j = 0; j < routingTable.size(); ++j) {
+			if (routingTable[j].destination == dv[i].destination) {
+				found = true;
+				if (routingTable[j].cost > dv[i].cost + cost) {
+					routingTable[j].cost = dv[i].cost + cost;
+					routingTable[j].nextHop = id;
+					changed = true;
+				}
+			}
+		}
+		if (!found) {
+			route.cost = dv[i].cost;
+			route.destination = dv[i].destination;
+			route.nextHop = id;
+			routingTable.push_back(route);
+			changed = true;
+		}
+	}
+	return changed;
+}
+
+//************
+//addNeighbor- takes the destination and the cost
+//records them as neighbors and initializes them in the
+//routing table
+//***********
+void Node::addNeighbor(int node, int dist) {
+	Route temp;
+	temp.cost = dist;
+	temp.destination = node;
+	temp.nextHop = node;
+	neighbors.push_back(node);
+	routingTable.push_back(temp);
 }
